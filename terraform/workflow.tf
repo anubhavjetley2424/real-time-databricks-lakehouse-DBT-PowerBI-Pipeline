fb7404e5e0afedb7404296eda0642d76d116cccc
Anubhav Jetley <anubhav.jetley123@gmail.com>
2026-03-09 22:11:13 +1100

    fix: use singular environment block for provider compatibility

--- terraform/workflow.tf
+++ terraform/workflow.tf
@@ -1,23 +1,13 @@
 resource "databricks_job" "smart_pest_pipeline" {
   name = "Smart_Pest_End_to_End_Pipeline"
 
-  # Single source of truth for your notebooks and dbt code
   git_source {
     url      = "https://github.com/anubhavjetley2424/flick-pest-dbt.git"
     branch   = "main"
     provider = "gitHub"
   }
 
-  environments {
-    environment_key = "dbt_env"
-    spec {
-      client = "dbt"
-    }
-  }
-
-  # -----------------------------------------------------------------------
-  # TASK 1: Ingestion (Spark Streaming)
-  # -----------------------------------------------------------------------
+  # Task 1: Ingestion (Spark Streaming)
   task {
     task_key = "ingestion_streaming"
     
@@ -25,8 +15,6 @@ resource "databricks_job" "smart_pest_pipeline" {
       num_workers   = 1
       spark_version = "13.3.x-scala2.12"
       node_type_id  = "Standard_DS3_v2"
-
-      # Terraform injects the Event Hub key directly into the cluster memory
       spark_conf = {
         "spark.eventhub.connectionString" = azurerm_eventhub_namespace.smart_ns.default_primary_connection_string
       }
@@ -37,15 +25,14 @@ resource "databricks_job" "smart_pest_pipeline" {
     }
   }
 
-  # -----------------------------------------------------------------------
-  # TASK 2: dbt Transformation (Using Serverless SQL Warehouse)
-  # -----------------------------------------------------------------------
+  # Task 2: dbt Transformation (Using Serverless SQL Warehouse)
   task {
     task_key = "dbt_transformations"
     depends_on { task_key = "ingestion_streaming" }
 
-    # Link to the environment defined at the top of the resource
-    environment_key = "dbt_env" 
+    # Using the singular 'environment' block inside the task
+    # This is compatible with both older and newer provider versions
+    environment_key = "dbt_env"
 
     dbt_task {
       project_directory = "dbt_project"
@@ -56,9 +43,14 @@ resource "databricks_job" "smart_pest_pipeline" {
     }
   }
 
-  # -----------------------------------------------------------------------
-  # SCHEDULE: Runs at the top of every hour
-  # -----------------------------------------------------------------------
+  # Define the environment at the job level (Singular 'environment' block)
+  environment {
+    environment_key = "dbt_env"
+    spec {
+      client = "dbt"
+    }
+  }
+
   schedule {
     quartz_cron_expression = "0 0 * * * ?"
     timezone_id            = "Australia/Sydney"
